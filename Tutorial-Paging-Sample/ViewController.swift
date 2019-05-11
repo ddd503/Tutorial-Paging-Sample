881c425c4380b60656d2067acfadb8de01b279ed
ddd503 <skbsporta7@gmail.com>
2019-05-11 16:07:04 +0900

    Initial Commit

---
+++ Tutorial-Paging-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorial-Paging-Sample
+//
+//  Created by kawaharadai on 2019/05/11.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
